b8d9c0bc3954483638c9c52188f4d55c75d21753
IT BT <itbtdeveloper.123@gmail.com>
2026-07-02 08:53:12 +0700

    Updated code

--- public/patch_run.php
+++ public/patch_run.php
@@ -3,20 +3,11 @@
 $filePath = dirname(__DIR__) . '/admin/settings.php';
 $content = file_get_contents($filePath);
 
-$pos = strpos($content, 'document.execCommand(cmd');
-if ($pos !== false) {
-    echo "Found document.execCommand(cmd at position $pos.<br>";
-    $start = max(0, $pos - 150);
-    $length = 300;
-    $slice = substr($content, $start, $length);
-    echo "Slice:<br><pre>" . htmlspecialchars($slice) . "</pre><br>";
-    echo "Hex representation of slice:<br><pre>";
-    for ($i = 0; $i < strlen($slice); $i++) {
-        $c = $slice[$i];
-        echo sprintf("%02X ", ord($c));
-        if (($i + 1) % 16 === 0) echo "\n";
-    }
-    echo "</pre>";
-} else {
-    echo "document.execCommand(cmd not found in settings.php";
+echo "<h3>All execCommand occurrences:</h3><pre>";
+$offset = 0;
+while (($pos = strpos($content, 'execCommand', $offset)) !== false) {
+    echo "Found execCommand at position $pos: \n";
+    echo htmlspecialchars(substr($content, max(0, $pos - 40), 100)) . "\n\n";
+    $offset = $pos + 11;
 }
+echo "</pre>";
